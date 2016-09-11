3ece30a735855ebca24c74923de010825383b7dd
Glen Costigan <gcostiga@vols.utk.edu>
2016-09-11 12:51:09 -0400

    Added code for piCamera

---
+++ STAB_1_code/piCamera/README.md
@@ -0,0 +1,7 @@
+# Pi Camera Config
+
+Run `sudo raspi-config` and choose in the menu to enable the pi camera. A reboot is needed after this.
+
+# Installation
+
+The `python-picamera` library is available in the Raspbian archives. Install with `apt`:
---
+++ STAB_1_code/piCamera/startRecording.py
@@ -0,0 +1,17 @@
+import time
+import picamera
+camera = picamera.PiCamera()
+
+time = time.strftime("%H_%M_%S")
+date = time.strftime("%Y_%m_%d")
+
+filename = time + 'EST' + date + '.h264'
+
+camera.resolution = (1280, 720) #1280x720
+camera.framerate = 60 #frames/sec
+recordingDuration = 5 #seconds
+
+#Start filming...
+camera.start_recording(filename)
+camera.wait_recording(recordingDuration)
+camera.stop_recording()
